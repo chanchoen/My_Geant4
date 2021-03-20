bf8eec797c4c3453cb1284c38d0ce31c90218c71
chanchoen <75157550+chanchoen@users.noreply.github.com>
2021-03-20 16:57:37 +0900

    Create RunAction.cc
    in this file declared three Ntuple with analysis 'electron energy', 'generate', 'step length'. 
    'electron energy' get to Track.cc file and add Ntuple 
    'generate' get to eventaction.cc file and add Ntuple 
    'step length' get to steppingaction file and add Ntuple
    
    and then end of runaction, should close the file.

---
+++ Modular_Tracking/RunAction.cc
@@ -0,0 +1,43 @@
+#include "OTRunAction.hh"
+
+OTRunAction::OTRunAction()
+: G4UserRunAction()
+{
+  fName = "data";
+}
+
+OTRunAction::OTRunAction(const char *name)
+: G4UserRunAction()
+{
+  fName = name;
+}
+
+OTRunAction::~OTRunAction()
+{
+  delete G4AnalysisManager::Instance();
+}
+
+void OTRunAction::BeginOfRunAction(const G4Run*)
+{
+  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+  analysisManager -> OpenFile(fName);
+
+  analysisManager -> CreateNtuple("electron_energy", "electron_energy");
+  analysisManager -> CreateNtupleDColumn("energy");
+  analysisManager -> FinishNtuple();
+
+  analysisManager -> CreateNtuple("generate_electron", "generate_electron");
+  analysisManager -> CreateNtupleIColumn("generate_count");
+  analysisManager -> FinishNtuple();
+
+  analysisManager -> CreateNtuple("step_length", "step_length");
+  analysisManager -> CreateNtupleDColumn("StepLength");
+  analysisManager -> FinishNtuple();
+}
+
+void OTRunAction::EndOfRunAction(const G4Run*)
+{
+  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+  analysisManager -> Write();
+  analysisManager -> CloseFile();
+}
